63b9494f8f53b028ebb001fe05aecf5af41140f6
Jake Bowen <jakeeb05@gmail.com>
2024-11-09 17:09:26 -0800

    Initial Commit

---
+++ Spaceship/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Spaceship
+//
+//  Created by Jake Bowen on 11/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
