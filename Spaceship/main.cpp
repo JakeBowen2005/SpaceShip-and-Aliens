e725253d772a85b2d7033beee02cb2e071e8acc1
Jake Bowen <jakeeb05@gmail.com>
2024-11-09 18:05:49 -0800

    Added both sprites

--- Spaceship/main.cpp
+++ Spaceship/main.cpp
@@ -5,10 +5,74 @@
 //  Created by Jake Bowen on 11/9/24.
 //
 
+//cd Desktop/SFML/Spaceship/Spaceship
+//g++ -o main main.cpp -I/opt/homebrew/include -L/opt/homebrew/lib -lsfml-graphics -lsfml-window -lsfml-system
+
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+using namespace sf;
+using namespace std;
+
+#define screen_width 640
+#define screen_length 480
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    srand(time_t(NULL));
+    
+    RenderWindow window(VideoMode(screen_width, screen_length), "SpaceShip");
+    window.setFramerateLimit(60);
+    
+    //Spaceship
+    Texture SpaceshipTex;
+    Sprite Spaceship;
+    
+    if (!SpaceshipTex.loadFromFile("Textures/spaceship.png")) {
+        cout <<  "Could not load spaceship from file";
+    }
+    Spaceship.setTexture(SpaceshipTex);
+    vector<Sprite> ships;
+    Spaceship.setScale(Vector2f(0.09f, 0.09f));
+    ships.push_back(Spaceship);
+    
+    
+    //Alien
+    Texture AlienTex;
+    Sprite Alien;
+    
+    if (!AlienTex.loadFromFile("Textures/aliens.png")) {
+        cout << "Could not load aliens from file";
+    }
+    Alien.setTexture(AlienTex);
+    vector<Sprite> aliens;
+    Alien.setScale(Vector2f(0.05f, 0.05f));
+    Alien.rotate(270.f);
+    aliens.push_back(Alien);
+    
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+        }
+        //Update
+        
+        
+        
+        //Draw
+        window.clear();
+        for (size_t i = 0; i < ships.size(); i++) {
+            window.draw(Spaceship);
+        }
+        for (size_t i = 0; i < aliens.size(); i++) {
+            window.draw(Alien);
+        }
+        
+        window.display();
+    }
 }
